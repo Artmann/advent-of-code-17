53d07f342ebc32818327d21b71c9853a62352380
Christoffer Artmann <christoffer@artmann.glesys.se>
2017-12-06 18:12:22 +0100

    Test Day6.recycle_memory_until_duplicate_state_is_found

--- lib/day6_spec.rb
+++ lib/day6_spec.rb
@@ -19,4 +19,12 @@ RSpec.describe Day6 do
       expect(memory_banks).to eq([2, 4, 1, 2])
     end
   end
+
+  describe "#recycle_memory_until_duplicate_state_is_found" do
+      it "returns the number of cycles and the first duplicate state" do
+        cycles, state = Day6.new.recycle_memory_until_duplicate_state_is_found([0, 2, 7, 0])
+        expect(cycles).to eq(5)
+        expect(state).to eq([2, 4, 1, 2])
+      end
+  end
 end
